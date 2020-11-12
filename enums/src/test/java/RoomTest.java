276d7a44617726bf27bda3a66a1cf2c6968ffd2e
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-11-12 09:58:18 +0000

    finished enums practice

---
+++ enums/src/test/java/RoomTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoomTest {
+
+    Room room;
+
+    @Before
+    public void before() {
+        room = new Room(RoomType.DOUBLE);
+    }
+
+    @Test
+    public void canGetRoomType() {
+        assertEquals(RoomType.DOUBLE, room.getRoomType());
+    }
+
+    @Test
+    public void doubleHasTheValue2() {
+        assertEquals(2, room.getValueFromEnum());
+    }
+
+
+
+
+}
